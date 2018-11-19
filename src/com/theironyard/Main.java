ddb9fc30fc5536ce1e8c71457b4909c299f913b2
Krista Blachian <krista.blachian@scifusion360.com>
2018-11-19 15:04:47 -0500

    exercise complete

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -15,8 +15,13 @@ public class Main {
         System.out.println(tim);
         tim.setWeapon("Stormbringer");
         saveObject(tim);
-        loadObject(tim);
+//        loadObject(tim);
         System.out.println(tim);
+
+        ISaveable werewolf = new Monster("Werewolf", 20,40);
+        System.out.println("Strength = " + ((Monster) werewolf).getStrength());
+        System.out.println(werewolf);
+        saveObject(werewolf);
 
     }
 
